e7969133d2fe0ab055ef3527361f39231749e839
Ben Gamari <ben@smart-cactus.org>
2019-03-10 17:11:20 -0400

    push-labels: Fix it

--- label-config/push-labels.py
+++ label-config/push-labels.py
@@ -4,18 +4,19 @@ import sys
 import gitlab
 import yaml
 
-gl = gitlab.Gitlab.from_config('haskell-staging')
+gl = gitlab.Gitlab.from_config('haskell')
 proj = gl.projects.get(1)
 existing = proj.labels.list(all=True)
-existing = { label.name: label for label in labels }
+existing = { label.name: label for label in existing }
 
 for label in yaml.load(open('labels.yaml')):
     print(label)
-    if label.name in existing:
-        l =  existing[label.name]
-        l.color = label.color
-        l.description = label.description
+    if label['name'] in existing:
+        l =  existing[label['name']]
+        l.color = label['color']
+        l.description = label['description']
         l.save()
     else:
-        proj.labels.create(label)
+        #proj.labels.create(label)
+        print("doesn't exist")
 
