7e80afd0881abec995f1d83225699c7d1af8939b
Ben Gamari <ben@smart-cactus.org>
2019-03-10 14:18:38 -0400

    label-config: Yet again fix it

--- label-config/default.nix
+++ label-config/default.nix
@@ -1,10 +1,10 @@
-{ stdenv, wrapPython, python-gitlab }:
+{ stdenv, wrapPython, python-gitlab, pyyaml }:
 
 stdenv.mkDerivation {
   name = "label-config";
   src = ./.;
   nativeBuildInputs = [ wrapPython ];
-  pythonPath = [ python-gitlab ];
+  pythonPath = [ pyyaml python-gitlab ];
   installPhase = ''
     install -D -t $out/bin dump-labels.py
     install -D -t $out/bin push-labels.py
