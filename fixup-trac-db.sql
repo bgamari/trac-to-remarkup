4ef0ab5680494f2eee1bb073e5ece290108c5234
Ben Gamari <ben@smart-cactus.org>
2019-03-10 10:23:01 -0400

    Fix fixup trac

--- fixup-trac-db.sql
+++ fixup-trac-db.sql
@@ -1,8 +1,8 @@
-DELETE FROM ticket_changes
+DELETE FROM ticket_change
 WHERE field = 'description'
   AND ticket = 2074;
 
-DELETE FROM ticket_changes
+DELETE FROM ticket_change
 WHERE field = 'description'
   AND ticket = 1436;
 
